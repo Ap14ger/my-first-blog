3a606f145bf730fc0b93024c3dd996f309ddd7a6
Gerson <occoner24023@gmail.com>
2026-04-11 21:07:21 -0500

    Añadi templates y tambien realice la primer modificacion en urls,views,y urls en mysite

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. es donde va la logica
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
