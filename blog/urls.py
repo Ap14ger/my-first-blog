07bc82ed69a20a76a890d32c1383cb04f47723bc
Gerson <occoner24023@gmail.com>
2026-06-17 21:37:33 -0500

    Agregadas vistas y plantillas para ver el detalle de cada publicacion en el blog y tambien se coloco una platilla base de la cual las otras heredan los estilos css para el sitio web.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # trae de views el metodo post_list
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
